809c8bce0717d65606c2f53fb0bedbd2033e6f3e
james-li-uchicago <james@LAPTOP-U9BPCALU.localdomain>
2020-08-29 13:55:08 -0500

    updates to master_script.R and README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ISTP-Statistics-2020
+
+README for Running the Code
+TL;DR: Download all the files on the GitHub repository and use the “Instructions for running code using GitHub files” to run Step 1 and Step 2 in the code (pre-processing steps) rather than directly running the code in the master_script.R file. Make sure to run these specific instructions before running any of the subsequent analysis in Steps 3 to 6!
+
+Description and background for our files:
+Step 1 and Step 2: these include the initial processing and annotation steps of our data. These steps are really computationally intensive and our GitHub repository only contains data files that are already processed in order to save run time. Here are Google Drive links to the raw data files in case you would like to run them (EXTREMELY time intense in Step 2): 
+https://drive.google.com/file/d/1q08eXj0qvNMuH-LuVP6Fu6mSO-W01B-j/view?usp=sharing 
+https://drive.google.com/file/d/1fBdi0DzRu-jTROAQ5_80nuUH-x3NM9Ot/view?usp=sharing
+https://drive.google.com/file/d/1fBdi0DzRu-jTROAQ5_80nuUH-x3NM9Ot/view?usp=sharing
+The instructions below are essential to run code using only the GitHub files and you shouldn’t need to run any other commands within Steps 1 and 2 besides the ones listed below.
+
+Instructions for running code using GitHub files (run only these lines in order to bypass the extremely time intensive Steps 1 and 2): 
+Install and run all necessary packages using lines 11-36
+Load our central gene expression data table for all the patients using lines 66-68
+Load pairwise cancer cohort comparison data for every single gene using line 102
+Congrats on finishing Step 1 and 2! Now you can run the code normally and however you want from Step 3 and after!
--- master_script.R
+++ master_script.R
@@ -19,6 +19,8 @@
 #install.packages("data.table")
 #install.packages("party")
 #install.packages("gplots")
+#install.packages("tidyverse")
+#install.packages("Hmisc")
 library("dplyr")
 library("ggplot2")
 library("cowplot")
@@ -30,6 +32,8 @@ library("pROC")
 library("data.table")
 library("party")
 library("gplots")
+library("tidyverse")
+library("Hmisc")
 
 #import raw data and label samples with their cancer cohort, as well as common gene IDs
 raw_data<-fread("data.csv",header=T)
@@ -41,8 +45,8 @@ new_gene_labels <- rbind(tempmatrix, gene_labels)
 colnames(dt)<-new_gene_labels$V1
 
 #saving our annotated data table as an .RData file
-save(dt,file = "dt.RData")
-#alternatively saving our annotated data table as a pair of two .RData files which can be merged later
+#save(dt,file = "dt.RData")
+#alternatively saving our annotated data table as a pair of two .RData files which can be merged later in order to meet GitHub max individual file size
 dt_part1<-dt[1:round(nrow(dt)/2),]
 dt_part2<-dt[(round(nrow(dt)/2)+1):nrow(dt),]
 save(dt_part1,file = "dt_part1.RData")
@@ -56,12 +60,12 @@ save(dt_part2,file = "dt_part2.RData")
 #for each gene (p-values and expression fold differences)
 #################################################################################################
 
-#loading our annotated data table (from line 40) if needed
+#loading our annotated data table (from line 48) if needed
 #load("dt.RData")
-#alternatively loading our annotated data from the pair of split tables 
-#load("dt_part1.RData")
-#load("dt_part2.RData")
-#dt<-rbind(dt_part1,dt_part2)
+#alternatively loading our annotated data from the pair of split tables (from lines 52 and 53) 
+load("dt_part1.RData")
+load("dt_part2.RData")
+dt<-rbind(dt_part1,dt_part2)
 
 #code to calculate raw p-values, adjusted p-values, and fold differences in expression between breast and prostate cancer patients
 cancer_list<-c("BRCA","COAD","KIRC","LUAD","PRAD") 
@@ -94,8 +98,8 @@ save(expr_significance,file="expr_significance.RData")
 #Step 3: Building a Naive Bayes Classifier based on genes that were differentially expressed
 #################################################################################################
 
-#loading our annotated data table (from line 75) if needed
-#load("expr_significance.RData")
+#loading our annotated data table (from line 93) if needed
+load("expr_significance.RData")
 
 #filtering our dataset to only include genes that differentially expressed in the pair-wise
 #comparisons via the following two criteria:
@@ -126,6 +130,54 @@ feature_matrix<-dt %>% select(target_matrix_columns) %>% filter(cancer_cohort ==
 feature_matrix$cancer_cohort <- as.factor(feature_matrix$cancer_cohort)
 save(feature_matrix,file="feature_matrix.RData")
 
+#plotting confusion matrix function: courtesy of Cybernetic
+draw_confusion_matrix <- function(cm) {
+  
+  layout(matrix(c(1,1,2)))
+  par(mar=c(2,2,2,2))
+  plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+  title('CONFUSION MATRIX', cex.main=2)
+  
+  # create the matrix 
+  rect(150, 430, 240, 370, col='#3F97D0')
+  text(195, 435, 'Class1', cex=1.2)
+  rect(250, 430, 340, 370, col='#F7AD50')
+  text(295, 435, 'Class2', cex=1.2)
+  text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
+  text(245, 450, 'Actual', cex=1.3, font=2)
+  rect(150, 305, 240, 365, col='#F7AD50')
+  rect(250, 305, 340, 365, col='#3F97D0')
+  text(140, 400, 'Class1', cex=1.2, srt=90)
+  text(140, 335, 'Class2', cex=1.2, srt=90)
+  
+  # add in the cm results 
+  res <- as.numeric(cm$table)
+  text(195, 400, res[1], cex=1.6, font=2, col='white')
+  text(195, 335, res[2], cex=1.6, font=2, col='white')
+  text(295, 400, res[3], cex=1.6, font=2, col='white')
+  text(295, 335, res[4], cex=1.6, font=2, col='white')
+  
+  # add in the specifics 
+  plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
+  text(10, 85, names(cm$byClass[1]), cex=1.2, font=2)
+  text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex=1.2)
+  text(30, 85, names(cm$byClass[2]), cex=1.2, font=2)
+  text(30, 70, round(as.numeric(cm$byClass[2]), 3), cex=1.2)
+  text(50, 85, names(cm$byClass[5]), cex=1.2, font=2)
+  text(50, 70, round(as.numeric(cm$byClass[5]), 3), cex=1.2)
+  text(70, 85, names(cm$byClass[6]), cex=1.2, font=2)
+  text(70, 70, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
+  text(90, 85, names(cm$byClass[7]), cex=1.2, font=2)
+  text(90, 70, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
+  
+  # add in the accuracy information 
+  text(30, 35, names(cm$overall[1]), cex=1.5, font=2)
+  text(30, 20, round(as.numeric(cm$overall[1]), 3), cex=1.4)
+  text(70, 35, names(cm$overall[2]), cex=1.5, font=2)
+  text(70, 20, round(as.numeric(cm$overall[2]), 3), cex=1.4)
+}  
+
+
 #running a naive bayes classifier with 5-fold cross validation
 n_folds <- 5
 folds_i <- sample(rep(1:n_folds, length.out = nrow(feature_matrix)))
@@ -146,7 +198,8 @@ for(k in 1:n_folds){
   nb_pred_classes <- predict(classifier_nb, newdata = test_data, type = "class")
   
   #assess the accuracy of the classifier on the test data using a confusion matrix
-  print(confusionMatrix(nb_pred_classes,test_data$cancer_cohort))
+  nb_cm<-confusionMatrix(nb_pred_classes,test_data$cancer_cohort)
+  draw_confusion_matrix(nb_cm)
   
   #assess the accuracy of the classifier on the test data using a ROC (function defined above)
   ROC <- roc(predictor=nb_pred_probabilities[,1],
@@ -154,6 +207,7 @@ for(k in 1:n_folds){
   print(ROC$auc)
   plot(ROC,main="ROC Curve for Naive Bayes Classifier")
 }
+
 
 
 
@@ -182,7 +236,8 @@ for(k in 1:n_folds){
   predicted_prob<-sapply(predict(classifier_ctree, test_data,type="prob"),'[[',1) 
   
   #print confusion matrix
-  print(confusionMatrix(predicted_class,test_data$cancer_cohort))
+  ctree_cm<-confusionMatrix(predicted_class,test_data$cancer_cohort)
+  draw_confusion_matrix(ctree_cm)
   
   #plot ROC curve
   ROC <- roc(predictor=predicted_prob,
@@ -198,29 +253,163 @@ for(k in 1:n_folds){
 #Step 5: Determining how long each model takes to get trained and how long each model takes to 
 #predict classes of test data 
 #################################################################################################
-#naive bayes times
-start.time <- Sys.time()
-classifier_nb <- naiveBayes(train_data[,-1], train_data$cancer_cohort)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(paste("Time to train Naive Bayes Classifier:",time.taken,"seconds"))
+#sampling some run time data
+if (exists("run_time_data")==TRUE) {
+  rm(run_time_data)
+}
+run_time_data<-data.table(Train_Naive_Bayes=as.numeric(), Test_Naive_Bayes=as.numeric(), Train_Decision_Tree=as.numeric(), Test_Decision_Tree=as.numeric())
+
+for (i in 1:20) {
+  #naive bayes times
+  start.time <- Sys.time()
+  classifier_nb <- naiveBayes(train_data[,-1], train_data$cancer_cohort)
+  end.time <- Sys.time()
+  time.taken_train_NB <- end.time - start.time
+  Train_Naive_Bayes <- as.numeric(time.taken_train_NB)
+  print(paste("Time to train Naive Bayes Classifier:",Train_Naive_Bayes,"seconds"))
   
-start.time <- Sys.time()
-nb_pred_classes <- predict(classifier_nb, newdata = test_data, type = "class")
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(paste("Time to predict classes of test data Using Naive Bayes Classifier:",time.taken,"seconds"))
+  start.time <- Sys.time()
+  nb_pred_classes <- predict(classifier_nb, newdata = test_data, type = "class")
+  end.time <- Sys.time()
+  time.taken_test_NB <- end.time - start.time
+  Test_Naive_Bayes <- as.numeric(time.taken_test_NB)
+  print(paste("Time to predict classes of test data Using Naive Bayes Classifier:",Test_Naive_Bayes,"seconds"))
   
-#decision tree times
-start.time <- Sys.time()
-classifier_ctree <- ctree(cancer_cohort ~ ., data=train_data)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(paste("Time to train Decision Tree:",time.taken,"seconds"))
+  #decision tree times
+  start.time <- Sys.time()
+  classifier_ctree <- ctree(cancer_cohort ~ ., data=train_data)
+  end.time <- Sys.time()
+  time.taken_train_ctree <- end.time - start.time
+  Train_Decision_Tree <- as.numeric(time.taken_train_ctree)
+  print(paste("Time to train Decision Tree:",Train_Decision_Tree,"seconds"))
   
-start.time <- Sys.time()
-classifier_ctree <- ctree(cancer_cohort ~ ., data=train_data)
-predicted_class<-predict(classifier_ctree, test_data, type="response")
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(paste("Time to predict classes of test data Using Decision Tree Model:",time.taken,"seconds"))
+  start.time <- Sys.time()
+  classifier_ctree <- ctree(cancer_cohort ~ ., data=train_data)
+  predicted_class<-predict(classifier_ctree, test_data, type="response")
+  end.time <- Sys.time()
+  time.taken_test_ctree <- end.time - start.time
+  Test_Decision_Tree <- as.numeric(time.taken_test_ctree)
+  print(paste("Time to predict classes of test data Using Decision Tree Model:",Test_Decision_Tree,"seconds"))
+  
+  temp_run_time_data<-data.table(Train_Naive_Bayes,Test_Naive_Bayes,Train_Decision_Tree,Test_Decision_Tree)
+  run_time_data<-rbind(run_time_data,temp_run_time_data)
+}
+
+#####plotting run times#####
+train_times<-run_time_data %>% select(Train_Naive_Bayes,Train_Decision_Tree)
+test_times<-run_time_data %>% select(Test_Naive_Bayes,Test_Decision_Tree)
+require(reshape2) 
+
+#plot model train times
+train_times_melt<-melt(train_times)
+colnames(train_times_melt)<-c("Stage_in_Model","Time_in_seconds")
+ggplot(data = train_times_melt, aes(x=Stage_in_Model, y=Time_in_seconds)) + geom_boxplot(aes(fill=Stage_in_Model)) + theme(legend.position="none") + ggtitle("Boxplots of Time in Seconds it takes to train Naive Bayes vs Decision Tree Models (n=20)")
+
+#plot model test times
+test_times_melt<-melt(test_times)
+colnames(test_times_melt)<-c("Stage_in_Model","Time_in_seconds")
+ggplot(data = test_times_melt, aes(x=Stage_in_Model, y=Time_in_seconds)) + geom_boxplot(aes(fill=Stage_in_Model)) + theme(legend.position="none") + ggtitle("Boxplots of Time in Seconds it takes to test Naive Bayes vs Decision Tree Models (n=20)")
+
+
+
+
+#################################################################################################
+#Step 6: Visualizing trends in the gene expression data
+#################################################################################################
+
+# PLOTS OF ORIGINAL DATA (BRCA VS. LUAD)
+# Plot expression fold difference against p value.
+ggplot(brca_luad, aes(x=adj_pval, y=expr_fold)) + 
+  geom_point(shape=1) +
+  geom_hline(yintercept=log(3,base=2)) +
+  geom_hline(yintercept=-log(3,base=2)) +
+  geom_vline(xintercept=.05) + 
+  scale_x_log10() +
+  labs(x="Adjusted p-value", y="log2[Expression Fold Difference]") +
+  theme()
+
+# Prepare violin plots of expression of selected genes among patients.
+brca_dt<-dt[dt$cancer_cohort=="BRCA",]
+luad_dt<-dt[dt$cancer_cohort=="LUAD",]
+combined_brcaluad_dt<-rbind(brca_dt,luad_dt)
+
+# The first step is to convert data in combined_brcaluad_dt to a more useful format.
+
+# Create vector (genes) of genes of interest.
+# Create vector (subset_names) to store names of data tables for each gene.
+# Create data.table (data_to_plot) which will contain all the data to plot.
+# Individual data subsets will be sequentially added to data_to_plot.
+genes <- c("KRAS","EGFR","MET","RET","ROS1")
+subset_names <- rep("",length(genes))
+data_to_plot = data.table(cancer_cohort = character(), 
+                          expression_level = numeric(), gene = character())
+
+# Summary: For each gene of interest in genes, 
+# we add relevant data from combined_brcaluad_dt to data_to_plot.
+# Details: 
+#   indices is a vector containing the columns of interest,
+#       specifically cancer_cohort and (expression level of) genes[i].
+#   subset_names[i] is updated to contain the correct name of the 
+#       current subset we are working with.
+#   A data.table is created with name subset_names[i] and three columns:
+#       cancer_cohort, (expression level of) genes[i], and a new column (gene).
+#       The new column contains the name of the current gene (genes[i]).
+#       cbind() combines the first two columns with the new third column.
+#   The second column is renamed from genes[i] to "expression_level"
+#       which is necessary for consistency.
+#       eval(str2lang()) evaluates subset_names[i] as the name of a data.table.
+#   The data.table subset_names[i] is added to data_to_plot with rbind().
+#       Again, eval(str2lang()) is used.
+for(i in 1:length(genes)){
+  indices <- c(2, grep(paste("\\b", genes[i], "\\b", sep=""), 
+                       colnames(combined_brcaluad_dt)))
+  subset_names[i] <- paste("subset_", genes[i], sep="")
+  assign(subset_names[i], 
+         cbind(combined_brcaluad_dt[,..indices],
+               data.table(gene = rep(genes[i],nrow(combined_brcaluad_dt)))))
+  setnames(eval(str2lang((subset_names[i]))), genes[i], "expression_level")
+  data_to_plot <- rbind(data_to_plot, eval(str2lang((subset_names[i]))))
+}
+
+# Now that the data in data_to_plot is in a useful format to make violin plots,
+# i.e. three columns with gene name and expression level as separate variables,
+# we can proceed with creating violin plots.
+windows(11,5)
+plot_violin <- ggplot(data_to_plot, aes(x=gene, y=expression_level, fill=cancer_cohort)) + 
+  geom_violin(trim=TRUE, position=position_dodge(1)) + 
+  labs(x="Gene of Interest", y="Normalized Expression (a.u.)",
+       title="Distributions in Gene Expression among Patients",
+       fill="Cancer Type") +
+  geom_boxplot(width=0.1, color="black", position=position_dodge(1)) +
+  theme_classic() +
+  theme(legend.position="right", plot.title = element_text(hjust = 0.5))
+plot_violin
+
+# Repeat the same process as above for a second set of genes:
+genes <- c("ATM","BARD1","BRIP1","CDH1","CHEK2","NBN","NF1","PALB2","PTEN", 
+           "RAD51C","RAD51D","STK11","TP53")
+subset_names <- rep("",length(genes))
+data_to_plot = data.table(cancer_cohort = character(), 
+                          expression_level = numeric(), gene = character())
+
+for(i in 1:length(genes)){
+  indices <- c(2, grep(paste("\\b", genes[i], "\\b", sep=""), 
+                       colnames(combined_brcaluad_dt)))
+  subset_names[i] <- paste("subset_", genes[i], sep="")
+  assign(subset_names[i], 
+         cbind(combined_brcaluad_dt[,..indices],
+               data.table(gene = rep(genes[i],nrow(combined_brcaluad_dt)))))
+  setnames(eval(str2lang((subset_names[i]))), genes[i], "expression_level")
+  data_to_plot <- rbind(data_to_plot, eval(str2lang((subset_names[i]))))
+}
+
+windows(11,5)
+plot_violin <- ggplot(data_to_plot, aes(x=gene, y=expression_level, fill=cancer_cohort)) + 
+  geom_violin(trim=TRUE, position=position_dodge(1)) + 
+  labs(x="Gene of Interest", y="Normalized Expression (a.u.)",
+       title="Distributions in Gene Expression among Patients",
+       fill="Cancer Type") +
+  geom_boxplot(width=0.1, color="black", position=position_dodge(1)) +
+  theme_classic() +
+  theme(legend.position="right", plot.title = element_text(hjust = 0.5))
+plot_violin
